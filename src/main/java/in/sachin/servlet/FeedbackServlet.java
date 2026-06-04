64ce7e4e4374bec100e691909d8b46f99adf23fc
Sachin Gupta <sachingu1602@gmail.com>
2026-06-04 13:40:11 +0530

    changes done

--- src/main/java/in/sachin/servlet/FeedbackServlet.java
+++ src/main/java/in/sachin/servlet/FeedbackServlet.java
@@ -12,6 +12,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+System.out.println("Hello");
+
 @WebServlet("/feedback")
 public class FeedbackServlet extends HttpServlet {
 
